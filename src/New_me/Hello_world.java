daef692778b3119022e4b22d4b9b3f63c5cfd81c
prath <prathmeshbhoge.cool@gmail.com>
2025-06-15 16:43:32 +0530

    Refactor

---
+++ src/New_me/Hello_world.java
@@ -0,0 +1,12 @@
+package New_me;
+
+public class Hello_world {
+    public static void main(String[] args) {
+        System.out.println(hello("Hellow world"));
+    }
+
+   public static  String hello(String s){
+            return s;
+
+    }
+}
